617ccb83b04306717bfcfcc9ea827694d71166b2
Joel Miguel <joel.miguel@utec.edu.pe>
2024-10-07 16:18:13 -0500

    acabado

---
+++ src/main/java/org/example/usermanagement/CordConfig/WebConfig.java
@@ -0,0 +1,16 @@
+package org.example.usermanagement.CordConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir CORS en todas las rutas
+                .allowedOrigins("http://localhost:5173") // Cambia esto a tu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+}
