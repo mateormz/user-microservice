7644b3cc846c6135ce79cdac2356f271f68dbbc0
Joel Miguel <joel.miguel@utec.edu.pe>
2024-10-07 17:25:02 -0500

    final

--- src/main/java/org/example/usermanagement/CorsConfig/WebConfig.java
+++ src/main/java/org/example/usermanagement/CorsConfig/WebConfig.java
@@ -1,4 +1,4 @@
-package org.example.usermanagement.CordConfig;
+package org.example.usermanagement.CorsConfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
