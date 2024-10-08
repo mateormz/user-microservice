fd2125cf476c65b30ecf1b974c583102ca362c0b
Joel Miguel <joel.miguel@utec.edu.pe>
2024-10-07 23:55:00 -0500

    final

--- src/main/java/org/example/usermanagement/CorsConfig/WebConfig.java
+++ src/main/java/org/example/usermanagement/CorsConfig/WebConfig.java
@@ -9,7 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permitir CORS en todas las rutas
-                .allowedOrigins("http://localhost:5173") // Cambia esto a tu frontend
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://reserva-canchas.s3-website-us-east-1.amazonaws.com"
+                ) // Especifica los orígenes permitidos
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
